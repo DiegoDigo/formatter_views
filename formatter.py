d7637e116176ebe90972d397c05d6d96071ce81b
Diego <di3g0d0ming05@gmail.com>
2018-03-26 16:21:44 -0300

    salvando views e procedures no banco de dados

--- formatter.py
+++ formatter.py
@@ -1,12 +1,14 @@
 import os
+import sys
 from re import findall, sub
 from configparser import ConfigParser
 from pathlib import Path
 from resouces import resouce
-# from infra import dao
+from infra import dao
 
 
 def criar_diretorio_destino(destino: str) -> None:
+    """ cria a estrutura de pasta que vem do arquivo de configuração. """
     path = Path(destino)
     if not path.exists():
         path.mkdir(parents=True, exist_ok=True)
@@ -16,39 +18,39 @@ def criar_diretorio_destino(destino: str) -> None:
 
 
 def pegar_arquivos(caminho: str) -> list:
-    """ lista todos os arquivos do diretorio """
+    """ lista todos os arquivos do diretorio. """
     return os.listdir(caminho)
 
 
 def pegar_nomes(texto: str) -> list:
-    """ retorna os nomes das tabelas """
+    """ retorna os nomes das tabelas. """
     return findall(r"(?:[\.\s])([a-zA-Z]+\d{2})(?:[\.\s])", texto)
 
 
 def gravar_arquivo(caminho: str, texto: str) -> None:
-    """ grava o arquivo editado """
+    """ grava o arquivo editado. """
     with open(caminho, "w", encoding="iso-8859-1") as arq:
         arq_final = sub(r"PERM"+caminho_conf["num_empresa"] + "\s", "PERM09", trocar_schema(texto))
         arq.write(arq_final)
-    # dao.criar_view(trocar_schema(texto))
 
 
 def formatar_string(destino: str, texto: str, arquivo: str, tipo: str, num_empresa: int) -> None:
-    """ subistitui o nome da tabela pelo novo nome """
+    """ subistitui o nome da tabela para o padrão configurado. """
     for nome in pegar_nomes(texto):
         texto = sub(nome, nome[:-2] + num_empresa, texto)
     gravar_arquivo(os.path.join(os.path.join(destino, tipo), arquivo), texto)
 
 
 def ler_arquivo(destino: str, caminho: str, arquivo: str, tipo: str, num_empresa: int) -> None:
-    """ ler o arquivo  """
+    """ ler o arquivo e formata a string com o padrao de schama. """
     criar_diretorio_destino(destino)
     print("gerando o arquivo {}".format(arquivo))
     with open(os.path.join(caminho, arquivo), encoding="iso-8859-1") as arq:
         formatar_string(destino, arq.read(), arquivo, tipo, num_empresa)
 
 
-def trocar_schema(texto: str) -> str:    
+def trocar_schema(texto: str) -> str:
+    """  troca o schema das tabelas. """
     novo_texto = ''
     for line in texto.split('\n'):    
         for key in caminho_conf.keys():
@@ -58,13 +60,27 @@ def trocar_schema(texto: str) -> str:
     return novo_texto
 
 
+def salvar_dbmaker(dsn: str, tipo: str) -> None:
+    """ ler o diretorio do arquivos formatado e salva no dbmaker. """
+    print("Salvando {} dbmaker, aguarde...".format(tipo))
+    path_views = os.path.join(caminho_conf['destino'], tipo)
+    for view in os.listdir(path_views):
+        with open(os.path.join(path_views, view), 'r', encoding="iso-8859-1") as arq:
+            if tipo == "views":
+                dao.criar_view(arq.read(), dsn)
+            else:
+                dao.criar_procedure(arq.read(), dsn)
+
+
 if __name__ == "__main__":
     print("Lendo arquivos ..")
 
     pasta_views = os.path.join(
-        os.path.join(os.path.join(resouce.NOME_PASTA + "\\views", os.listdir(resouce.extrair_arquivo())[0])),'views')
+        os.path.join(os.path.join(resouce.NOME_PASTA + "\\views",
+                                  os.listdir(resouce.extrair_arquivo())[0])), 'views')
     pasta_prodeures = os.path.join(
-        os.path.join(os.path.join(resouce.NOME_PASTA + "\\views", os.listdir(resouce.extrair_arquivo())[0])), 'procedures')
+        os.path.join(os.path.join(resouce.NOME_PASTA + "\\views",
+                                  os.listdir(resouce.extrair_arquivo())[0])), 'procedures')
 
     config = ConfigParser()
     config.read('settings\config.ini')
@@ -75,5 +91,9 @@ if __name__ == "__main__":
 
     for arquivo in pegar_arquivos(pasta_prodeures):
         ler_arquivo(caminho_conf["destino"], pasta_prodeures, arquivo, "procedures", caminho_conf["num_empresa"])
+
+    salvar_dbmaker(caminho_conf['dsn'], 'views')
+
+    salvar_dbmaker(caminho_conf['dsn'], 'procedures')
 
     input("Press Any key to exit")
--- infra/dao.py
+++ infra/dao.py
@@ -1,20 +1,29 @@
 import pyodbc
-from configparser import ConfigParser
 
 
-config = ConfigParser()
-config.read('settings\config.ini')
-caminho = dict(config['CONFIG'])
-
-
-def criar_view(view: str):
+def criar_view(view: str, dsn: str) -> None:
+    """  Recria ou cria as views na base do DBMAKER baseado na conexao DSN. """
     try:
-        print(view)
-        _conn = pyodbc.connect('DSN={}'.format(caminho['dsn']))
+        _conn = pyodbc.connect('DSN={}'.format(dsn))
         _cur = _conn.cursor()
-        for sql in view.split(";\n"):
-            _cur.execute(sql)
-        _conn.commit()        
+        for sql in view.split(";"):
+            if sql != '\n':
+                if not sql.__contains__("create function"):
+                    _cur.execute(sql)
+                    _conn.commit()
+    except pyodbc.Error as e:
+        raise Exception(e)
+    finally:
+        _conn.close()
+
+
+def criar_procedure(procedure: str, dsn: str) -> None:
+    """  Recria ou cria as Procedures na base do DBMAKER baseado na conexao DSN. """
+    try:
+        _conn = pyodbc.connect('DSN={}'.format(dsn), autocommit=True)
+        _cur = _conn.cursor()
+        _cur.execute(procedure)
+        _conn.commit()
     except pyodbc.Error as e:
         raise Exception(e)
     finally:
